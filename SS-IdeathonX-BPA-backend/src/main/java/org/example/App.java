76aa5a1d0b294ddbdc12ffcb1e3ceefd200d172f
unknown <gpraveenk3@gmail.com>
2023-09-21 11:50:44 +0000

    first commit

---
+++ SS-IdeathonX-BPA-backend/src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+        System.out.println("Running our app. Hello World!!");
+    }
+
+    @Bean
+    public RouterFunction<?> routes(WelcomeHandler welcomeHandler, GreetingHandler greetingHandler) {
+        return welcomeHandler.routes()
+            .and(greetingHandler.routes());
+    }
+}
+
